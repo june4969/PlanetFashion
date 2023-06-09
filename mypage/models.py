1a2b94877ad6ee1ddbe50075eb4afcb0f4085309
june <blaare4869@gmail.com>
2023-06-10 00:51:22 +0900

    update

--- mypage/models.py
+++ mypage/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
-from shop.models import Product
 
-class MyProduct(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.product.name
-
-
+        return self.title
