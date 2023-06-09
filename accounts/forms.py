b237975d400ad54e470ff5ae3916bfb3a64e2979
june <blaare4869@gmail.com>
2023-06-10 00:53:52 +0900

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+    username = forms.CharField(label='이름')
+    email = forms.CharField(label='이메일')
+    password = forms.CharField(label='비밀번호', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='비밀번호 확인', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password', 'password2']
+        fields = ['username', 'email', 'password', 'password2']
 
     def clean_password2(self):
         cd = self.cleaned_data
